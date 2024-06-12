d65c399a0c883ebcd9de8bbbf301cf7504aa3fe6
Amr Osman <amr@Amrs-iMac-Pro.local>
2024-06-12 18:34:50 +0300

    Port to MacOS

--- cmake/MacOs.cmake
+++ cmake/MacOs.cmake
@@ -1,10 +1,23 @@
-add_executable(${Target} MACOSX_BUNDLE ${SOURCES})
-find_package(wxWidgets REQUIRED COMPONENTS net core base aui)
+# Add wxWidgets as a dependency
+CPMAddPackage(
+  NAME
+  wxWidgets
+  URL
+  https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2
+  VERSION 3.2.5
+  HASH    "SHA1=da39a3ee5e6b4b0d3255bfef95601890afd80709"
+  OPTIONS "wxBUILD_SHARED OFF" "wxUSE_STL ON"
+)
 
-if(wxWidgets_USE_FILE) # not defined in CONFIG mode
-  include("${wxWidgets_USE_FILE}")
-endif()
+# Add the source files for your project
+add_executable(${Target} MACOSX_BUNDLE ${SOURCES})
+
+# Link wxWidgets to your project
+target_link_libraries(${Target} PRIVATE wx::net wx::core wx::base)
+
+# Include wxWidgets headers
+target_include_directories(${Target} PRIVATE ${wxWidgets_SOURCE_DIR}/include)
+
+set_target_properties(${Target} PROPERTIES MACOSX_BUNDLE_INFO_PLIST ${Bundle}/Info.plist)
+
 target_include_directories(${Target} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/Sources/")
-set_target_properties(${Target} PROPERTIES MACOSX_BUNDLE_INFO_PLIST
-                                           ${Bundle}/Info.plist)
-target_link_libraries(${Target} PRIVATE core net base aui)
