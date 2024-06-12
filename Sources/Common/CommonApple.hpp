522f29e31c7d76f07cca1a4ce6abfade93fe84c2
Dr. Amr Osman <dr3mro@gmail.com>
2024-06-13 01:33:42 +0300

    update format

--- Sources/Common/CommonApple.hpp
+++ Sources/Common/CommonApple.hpp
@@ -9,14 +9,15 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 
-inline wxString getResourcesDirectoryPath() {
+inline wxString getResourcesDirectoryPath()
+{
     wxString resourcesPath;
     CFBundleRef mainBundle = CFBundleGetMainBundle();
     if (mainBundle) {
         CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
         if (resourcesURL) {
             char resourcePathChars[PATH_MAX];
-            if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)resourcePathChars, PATH_MAX)) {
+            if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8*)resourcePathChars, PATH_MAX)) {
                 resourcesPath = wxString::FromUTF8Unchecked(resourcePathChars);
             }
             CFRelease(resourcesURL);
