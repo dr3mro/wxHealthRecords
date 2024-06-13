2e2b2d60871d97ec91cba2335fca2b2f443eadc6
Dr Amr Osman <dr3mro@gmail.com>
2024-06-13 15:10:10 +0300

    Fix build on non apple

--- cmake/Vars.cmake
+++ cmake/Vars.cmake
@@ -1,2 +1,20 @@
 set(Bundle "${CMAKE_CURRENT_SOURCE_DIR}/Bundle/")
 file(GLOB_RECURSE SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/Sources/*")
+
+if(APPLE)
+  list(REMOVE_ITEM SOURCES
+       "${CMAKE_CURRENT_SOURCE_DIR}/Sources/SoundEffects/SoundEffectsImpl.cpp")
+  list(REMOVE_ITEM SOURCES
+       "${CMAKE_CURRENT_SOURCE_DIR}/Sources/SoundEffects/SoundEffectsImpl.hpp")
+else()
+  list(
+    REMOVE_ITEM SOURCES
+    "${CMAKE_CURRENT_SOURCE_DIR}/Sources/SoundEffects/SoundEffectsImplApple.cpp"
+  )
+  list(
+    REMOVE_ITEM SOURCES
+    "${CMAKE_CURRENT_SOURCE_DIR}/Sources/SoundEffects/SoundEffectsImplApple.hpp"
+  )
+  list(REMOVE_ITEM SOURCES
+       "${CMAKE_CURRENT_SOURCE_DIR}/Sources/Common/CommonApple.hpp")
+endif()
